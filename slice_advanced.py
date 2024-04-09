c70fa371e55fbe256709933b61b39eb53205b20a
francastro1 <162723525+francastro1@users.noreply.github.com>
2024-04-09 10:03:33 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,5 +2,3 @@ def slice_advanced():
     hello_world=input("Ingrese Hola, Mundo! en ingles ")
     print(hello_world[4:13:2])
 
-    numero=input("Ingrese los numeros del 1 al 10 incluido ")
-    print(numero[4:11:2])
