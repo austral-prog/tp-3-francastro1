658fbf7815c49022f75032bbfd6d9751332e0744
francastro1 <162723525+francastro1@users.noreply.github.com>
2024-04-09 14:22:58 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -11,16 +11,3 @@ def check_vowels():
     print(f"Contiene i: {i}")
     print(f"Contiene o: {o}")
     print(f"Contiene u: {u}")
-
-    name1=input("Ingrese su otro nombre ")
-    name1=name1.lower()
-    a1= "a" in name1
-    e1= "e" in name1
-    i1= "i" in name1
-    o1= "o" in name1
-    u1= "u" in name1
-    print(f"Contiene a: {a1}")
-    print(f"Contiene e: {e1}")
-    print(f"Contiene i: {i1}")
-    print(f"Contiene o: {o1}")
-    print(f"Contiene u: {u1}")
